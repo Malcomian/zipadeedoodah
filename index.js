9902ff7b3f04e10eea867e6e29745b64a15b2e80
Malcomian <37883015+Malcomian@users.noreply.github.com>
2021-02-23 16:14:31 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,130 @@
+#!/usr/bin/env node
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+var comment = ''
+rl.question('Comment for this archive (optional):', (answer) => {
+  if (answer) {
+    comment = answer
+  }
+  zip()
+  rl.close()
+})
+
+function zip() {
+  const start_time = new Date().getTime()
+
+  const Command = require('commander').Command
+  const program = new Command()
+
+  program.name('zipadeedoodah')
+  program.version(require('./package.json').version)
+
+  program.option('-o, --output <output>', 'Relative path of output file (no ext)')
+  program.option('-g, --globs <globs...>', 'Glob patterns')
+  program.option('-i, --ignores [ignores...]', 'Ignore patterns')
+  program.option('-d, --dot', 'Include dotfiles')
+  program.option('-l, --level [number]', 'compression level (0-9)')
+
+  program.parse(process.argv)
+
+  // get the options passed to the program
+  var opts = program.opts()
+
+  // initialize errors array
+  var errors = []
+
+  if (opts['output'] === undefined) errors.push('Please define an output file path!')
+  if (opts['globs'] === undefined) errors.push('Please define at least one glob pattern!')
+
+  // if there's any errors, log them and exit
+  if (errors.length > 0) {
+    errors.forEach((error) => console.log(error))
+    process.exit()
+  }
+
+  const path = require('path')
+  const bytes = require('bytes')
+  const fs = require('fs')
+  const archiver = require('archiver')
+
+  if (opts['level'] === undefined) opts['level'] = 9
+
+  var archive = archiver('zip', {
+    zlib: {
+      level: Number(opts['level'])
+    }
+  })
+
+  class Keyword {
+    constructor(name, value) {
+      this.name = name
+      this.value = value
+    }
+  }
+
+  var keywords = []
+  keywords.push(new Keyword('<timestamp>', getTimestamp()))
+  keywords.push(new Keyword('<cwd>', process.cwd().split('\\').pop()))
+
+  keywords.forEach((keyword) => {
+    if (opts['output'].includes(keyword.name)) opts['output'] = opts['output'].replace(keyword.name, keyword.value)
+  })
+
+  if (comment.length > 0) opts['output'] += ` - ${comment}`
+  opts['output'] += '.zip'
+
+  var output = fs.createWriteStream(opts['output'])
+
+  var directories = 0
+  var files = 0
+
+  console.log(`Archiving ${path.resolve(opts['output'])} at compression level ${opts['level']}...`)
+
+  archive.on('entry', (entry) => {
+    if (entry.stats.isFile()) files++
+    if (entry.stats.isDirectory()) directories++
+  })
+
+  archive.on('finish', () => {
+    let plural_files = ''
+    if (files > 1 || files == 0) plural_files = 's'
+    let plural_directories = 'y'
+    if (directories > 1 || directories == 0) plural_directories = 'ies'
+    console.log(`Archived ${files} file${plural_files} and ${directories} director${plural_directories}`)
+    var end_time = new Date().getTime()
+    var elapsed = end_time - start_time
+    console.log(`Wrote ${bytes(archive.pointer())} in ${elapsed}ms`)
+  })
+
+  archive.on('error', (error) => {
+    throw error
+  })
+
+  archive.pipe(output)
+
+  // initialize glob_config object to pass to the archive glob
+  var glob_config = {}
+  if (opts['ignores'] !== undefined) glob_config.ignore = opts['ignores']
+  if (opts['dot'] !== undefined) glob_config.dot = true
+
+  archive.glob(opts['globs'], glob_config)
+
+  // finalize file
+  archive.finalize()
+
+  function getTimestamp() {
+    let now = new Date().getTime()
+    let yyyy = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(now)
+    let mm = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(now)
+    if (mm < 10) mm = `0${mm}`
+    let dd = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(now)
+    let hour = new Intl.DateTimeFormat('en', { hour: 'numeric', hour12: false }).format(now)
+    let min = new Intl.DateTimeFormat('en', { minute: 'numeric' }).format(now)
+    let sec = new Intl.DateTimeFormat('en', { second: 'numeric' }).format(now)
+    return `${yyyy}-${mm}-${dd}_${hour}-${min}-${sec}`
+  }
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Zipadeedoodah
+
+A simple CLI zip utility based on glob patterns with minimal setup.
+
+This package can be installed globally and invoked with `zipadeedoodah` and requires a couple command line arguments.
+
+```bash
+npm install -g zipadeedoodah
+```
+
+This utility uses [Archiver](https://www.npmjs.com/package/archiver) with mostly just the basic options checked. It relies on just a few variadic user inputs based on glob patterns to include or ignore files and folders for the archive.
+
+## Usage
+
+```text
+  -V, --version               output the version number
+  -o, --output <output>       Relative path of output file (no ext)
+  -g, --globs <globs...>      Glob patterns
+  -i, --ignores [ignores...]  Ignore patterns
+  -d, --dot                   Include dotfiles
+  -l, --level [number]        compression level (0-9)
+  -h, --help                  display help for command
+```
+
+A couple of replacement keywords are available. `<cwd>`, which represents the name of the current working directory that the command was run in, and `<timestamp>`, which adds a timestamp in the format `yyyy-mm-dd_hour-min-sec`.
+
+The user is also prompted for a comment for the archive, which will be added to the end of the archive name.
+
+## Example
+
+Create an archive alongside the current directory with all files and folders of the current directory with the name of the current working directory followed by a timestamp, which excludes the `node_modules` folder, includes dotfiles, and archives at the highest compression level.
+
+```bash
+zipadeedoodah -o "../<cwd>_<timestamp>" -g "*/**" "*.*" -d -i "node_modules/**" -l 9
+```
